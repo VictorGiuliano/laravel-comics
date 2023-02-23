51bb165be22fbc30f7e0217504ea680bbc1cbfbb
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-23 19:32:54 +0100

    Add footer and use  layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,42 @@
+@extends('layouts.main')
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/js/app.js')
-    <title>Dc Comics</title>
+    @section('title', 'home')
 </head>
 <body>
-    <header>
-            <nav class="navbar">
-                <div class="navbar-logo container"><i class="fa-brands fa-discord"></i>
-                    <img src="{{asset('images/dc-logo.png')}}">
-                </div>
-                <div class="navbar-menu">
-                    <ul>
-                        <li><a href="#">CHARACTERS</a></li>
-                        <li><a href="#">COMICS</a></li>
-                        <li><a href="#">MOVIES</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">GAMES</a></li>
-                        <li><a href="#">COLLECTIBLES</a></li>
-                        <li><a href="#">VIDEOS</a></li>
-                        <li><a href="#">FANS</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">SHOP</a></li>
-                        <li><input type="text" placeholder="Search.."></li>
-                    </ul>
-                </div>
-            </nav>
-    </header>
     <main>
+        @section('content')
         <div class="Jumbotron"></div>
         <div class=" bkg-b container">
             <div class="label stick">
                 <h1>CURRENT SERIES</h1>
             </div>
             <div class="card-container">
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
                 <div class="comics-card">
                     <img src="" alt="comic.series">
                     <h4>AAA</h4>
@@ -49,6 +49,7 @@
         <section class="bonus">
             <div class=" bkg-bl"></div>
         </section>
+        @endsection
     </main>
 </body>
 </html>
