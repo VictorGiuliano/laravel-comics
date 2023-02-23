2ab897339ed647fd87be1001d3f6b4e61650de18
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-23 17:57:19 +0100

    Add Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,5 +30,25 @@
                 </div>
             </nav>
     </header>
+    <main>
+        <div class="Jumbotron"></div>
+        <div class=" bkg-b container">
+            <div class="label stick">
+                <h1>CURRENT SERIES</h1>
+            </div>
+            <div class="card-container">
+                <div class="comics-card">
+                    <img src="" alt="comic.series">
+                    <h4>AAA</h4>
+                </div>
+            </div>
+            <button class="btn btn-more">
+                <h1>LOAD MORE</h1>
+            </button>
+        </div>
+        <section class="bonus">
+            <div class=" bkg-bl"></div>
+        </section>
+    </main>
 </body>
 </html>
