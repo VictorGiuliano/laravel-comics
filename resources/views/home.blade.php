65a59452a185e0a5a52a6c7a74727ddf618f2378
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-23 21:19:52 +0100

    Ad section icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,8 +24,30 @@
                 <h1>LOAD MORE</h1>
             </button>
         </div>
-        <section class="bonus">
-            <div class=" bkg-bl"></div>
+        <section class="articles">
+            <div class="bkg-bl container">
+                <div class="icons">
+                    <img src="{{asset('images/buy-comics-digital-comics.png')}}">
+                </div>
+                <h3>DIGITAL COMICS</h3>
+                <div class="icons">
+                    <img src="{{asset('images/buy-comics-merchandise.png')}}">
+                </div>
+                <h3>DC MERCHANDISE</h3>
+                <div class="icons">
+                    <img src="{{asset('images/buy-comics-subscriptions.png')}}">
+                </div>
+                <h3>SUBSCRIPTION</h3>
+                <div class="icons">
+                    <img src="{{asset('images/buy-comics-shop-locator.png')}}">
+                </div>
+                <h3>COMIC SHOP LOCATOR</h3>
+                
+                <div class="icons">
+                    <img src="{{asset('images/buy-dc-power-visa.png')}}">
+                </div>
+                <h3>DC POWER VISA</h3>
+            </div>
         </section>
         @endsection
     </main>
